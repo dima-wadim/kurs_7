783f9a392b2fd75382846e36205bb37d2f137daf
Вадим <kantora_70@mail.ru>
2023-12-16 10:38:17 +0500

    Курс Docker

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from datetime import timedelta
-load_dotenv()
+import dotenv
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 dot_env = os.path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path=dot_env)
+#load_dotenv(dotenv_path=dot_env)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -180,6 +181,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
     "https://read-only.example.com",
     "https://read-and-write.example.com",
 ]
@@ -194,9 +196,7 @@ CHAT_ID = os.getenv('CHAT_ID')
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000'
-]
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15000),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=100),
